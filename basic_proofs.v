f2a080d24ecd11a41b5ccc7a6c11a37aa66ff7a6
Ethan Vizitei <ethan.vizitei@gmail.com>
2025-02-23 09:57:33 -0600

    first proof on late days

--- basic_proofs.v
+++ basic_proofs.v
@@ -25,11 +25,46 @@ Proof. intros b c H. destruct b.
   - simpl in H. rewrite H. reflexivity.
 Qed.
 
+Module LateDays.
 
+Inductive letter : Type :=
+  | A | B | C | D | F.
 
+Inductive modifier : Type :=
+  | Plus | Natural | Minus.
 
+Inductive grade : Type :=
+  Grade (l : letter) (m : modifier).
 
+Inductive comparison : Type :=
+  | Eq | Lt | Gt.
 
+Definition letter_comp (l1 l2 : letter) : comparison :=
+ match l1, l2 with
+  | A, A => Eq
+  | A, _ => Gt
+  | B, A => Lt
+  | B, B => Eq
+  | B, _ => Gt
+  | C, (A | B) => Lt
+  | C, C => Eq
+  | C, _ => Gt
+  | D, F => Gt
+  | D, D => Eq
+  | D, _ => Lt
+  | F, F => Eq
+  | F, _ => Lt
+ end.
 
+Theorem letter_comp_eq :
+  forall l : letter, letter_comp l l = Eq.
+Proof.
+  intros l. destruct l eqn:Eql.
+  - simpl. reflexivity.
+  - simpl. reflexivity.
+  - simpl. reflexivity.
+  - simpl. reflexivity.
+  - simpl. reflexivity.
+Qed.
 
-
+End LateDays.
