f4a29772c1e222999326dcdc47fa7daad7d3cb2f
Ethan Vizitei <ethan.vizitei@gmail.com>
2025-02-23 09:46:14 -0600

    and/or equality proof

--- basic_proofs.v
+++ basic_proofs.v
@@ -17,7 +17,13 @@ Qed.
 
 
 
-
+Theorem andb_eq_orb :
+  forall (b c : bool),
+    (andb b c = orb b c) -> b = c.
+Proof. intros b c H. destruct b.
+  - simpl in H. rewrite H. reflexivity.
+  - simpl in H. rewrite H. reflexivity.
+Qed.
 
 
 
